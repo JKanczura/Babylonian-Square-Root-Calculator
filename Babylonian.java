b2968b65c53a6f9804e4ba87ae9a128d9df66e6d
Jessica Kanczura <jkanczura@albany.edu>
2016-04-18 18:01:27 -0400

    Added files via upload

---
+++ Babylonian.java
@@ -0,0 +1,30 @@
+//Jessica Kanczura
+//ICSI 310
+//HW #5
+
+import java.util.Scanner;
+
+public class Babylonian
+{
+  private static double x = 0;
+  private static double val = 0;
+  
+  public static void main(String[] args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("What value would you like the square root of?");
+    x = keyboard.nextDouble();
+    val = x;
+    recursion(x);
+    keyboard.close();
+  }
+  
+  public static void recursion(double num)
+  {
+    x = 0.5 * (x + (val/x));
+    if (x * x == val)
+    System.out.println("Square root: " + x);
+    else
+      recursion(x);
+  }
+}
